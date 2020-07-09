3edef282d962a84bd42e41148b72507822e21281
opus1993 <jim.gruman@gmail.com>
2020-07-08 20:56:58 -0500

    GLMnet Manhattan

---
+++ content/post/GLMnet/code/glmnet.r
@@ -0,0 +1,78 @@
+library(glmnet)
+library(useful)
+library(coefplot)
+
+# Video at https://youtu.be/WWuW4kkI_SA
+
+land_train <- readRDS('data/manhattan_Train.rds')
+
+View(land_train)
+
+valueFormula <- TotalValue ~ FireService + 
+    ZoneDist1 + ZoneDist2 + Class + LandUse + 
+    OwnerType + LotArea + BldgArea + ComArea + 
+    ResArea + OfficeArea + RetailArea + 
+    GarageArea + FactryArea + NumBldgs + 
+    NumFloors + UnitsRes + UnitsTotal + 
+    LotFront + LotDepth + BldgFront + 
+    BldgDepth + LotType + Landmark + BuiltFAR +
+    Built + HistoricDistrict - 1
+
+value1 <- lm(valueFormula, data=land_train)
+value1
+summary(value1)
+coefplot(value1, sort='magnitude')
+
+landX_train <- build.x(valueFormula, data=land_train,
+                       contrasts=FALSE, sparse=TRUE)
+landY_train <- build.y(valueFormula, data=land_train)
+
+landX_train
+head(as.matrix(landX_train))
+
+
+value2 <- glmnet(x=landX_train, y=landY_train, family='gaussian')
+value2
+value2$beta
+View(as.matrix(value2$beta))
+
+plot(value2, xvar='lambda', label=TRUE)
+coefpath(value2)
+
+library(animation)
+cv.ani(k=10)
+
+# lasso regression, to eliminate features
+value3 <- cv.glmnet(x=landX_train, y=landY_train,
+                    family='gaussian',
+                    nfolds=5)
+plot(value3)
+coefpath(value3)
+# best model, by min error
+coefplot(value3, sort='magnitude', lambda='lambda.min')
+# smaller model, one standard deviation from min error
+coefplot(value3, sort='magnitude', lambda='lambda.1se')
+
+# ridge regression
+value4 <- cv.glmnet(x=landX_train, y=landY_train,
+                    family='gaussian',
+                    alpha=0,
+                    nfolds=5)
+coefpath(value4)
+coefplot(value4, sort='magnitude', lambda='lambda.1se')
+
+# so, how do we decide if we want lasso or ridge ?
+#   elastic net, for 60% lasso, 40% ridge
+value5 <- cv.glmnet(x=landX_train, y=landY_train,
+                    family='gaussian',
+                    alpha=0.6,
+                    nfolds=5)
+coefpath(value5)
+
+land_test <- readRDS('data/manhattan_Test.rds')
+landX_test <- build.x(valueFormula, data=land_test,
+                      contrasts=FALSE, sparse=TRUE)
+
+valuePredictions5 <- predict(value5, newx=landX_test, 
+                             s='lambda.1se')
+head(valuePredictions5)
